b496c5bbdd3fe61075acfed9d3d232dbb5d6690c
bcmediac15 <made@artha.web.id>
2020-07-09 07:37:00 +0800

    commit Update Contoh tahap pertama

--- data/data02.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $cnt = '{"employees":[{"NAMA":"Rohalia","KONTAK":"0361121212"},{"NAMA":"Sanusi Amir","KONTAK":"0361435456"},{"NAMA":"Made Gina Adiarsa","KONTAK":"0361555322"}]}';
-    header('Content-Type: application/json');
-    echo $cnt;
